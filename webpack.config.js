1bf4a6e419b20cdd7f24a0a0c87859578c5c72e5
AlanKev117 <fuentes.alan.98@gmail.com>
2020-11-06 13:06:08 -0600

    Babel, plugins and presets

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,11 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.js$/,
+                use: "babel-loader",
+                exclude: path.resolve(__dirname, "node_modules")
+            },
+            {
                 test: /\.css$/,
                 use: [
                     // MiniCssExtractPlugin.loader, /* Transpila archivos (producción) */
